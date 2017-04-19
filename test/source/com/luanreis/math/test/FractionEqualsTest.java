cea33f0e8a5982db37c46f27e07d9b03d086a8a6
Luan Reis <luan.rpsantos@gmail.com>
2017-04-19 18:53:45 +0200

    Refactor coding style to improve readability on tests

--- test/source/com/luanreis/math/test/FractionEqualsTest.java
+++ test/source/com/luanreis/math/test/FractionEqualsTest.java
@@ -10,26 +10,36 @@ public class FractionEqualsTest {
 
     @Test
     public void sameNumeratorAndDenominator() throws Exception {
-        assertEquals(new Fraction(3, 5), new Fraction(3, 5));
+        assertEquals(
+                new Fraction(3, 5),
+                new Fraction(3, 5));
     }
 
     @Test
     public void differentNumerators() throws Exception {
-        assertNotEquals(new Fraction(1, 5), new Fraction(2, 5));
+        assertNotEquals(
+                new Fraction(1, 5),
+                new Fraction(2, 5));
     }
 
     @Test
     public void differentDenominators() throws Exception {
-        assertNotEquals(new Fraction(3, 4), new Fraction(3, 7));
+        assertNotEquals(
+                new Fraction(3, 4),
+                new Fraction(3, 7));
     }
 
     @Test
     public void wholeNumberEqualsSameFraction() throws Exception {
-        assertEquals(new Fraction(5, 1), new Fraction(5));
+        assertEquals(
+                new Fraction(5, 1),
+                new Fraction(5));
     }
 
     @Test
     public void wholeNumberNotEqualToDifferentWholeNumber() throws Exception {
-        assertNotEquals(new Fraction(6), new Fraction(5));
+        assertNotEquals(
+                new Fraction(6),
+                new Fraction(5));
     }
 }
