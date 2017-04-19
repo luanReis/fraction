9eadcb83f440ec324f443f71b835694548b34840
Luan Reis <luan.rpsantos@gmail.com>
2017-04-19 19:01:49 +0200

    Remove scaffolding from all test assertions

--- test/source/com/luanreis/math/test/AddFractionsTest.java
+++ test/source/com/luanreis/math/test/AddFractionsTest.java
@@ -8,44 +8,50 @@ import static org.junit.Assert.assertEquals;
 public class AddFractionsTest {
     @Test
     public void zeroPlusZero() throws Exception {
-        Fraction sum = new Fraction(0).add(new Fraction(0));
-        assertEquals(0, sum.intValue());
+        assertEquals(
+                new Fraction(0),
+                new Fraction(0).add(new Fraction(0)));
     }
 
     @Test
     public void nonZeroPlusZero() throws Exception {
-        Fraction sum = new Fraction(3).add(new Fraction(0));
-        assertEquals(3, sum.intValue());
+        assertEquals(
+                new Fraction(3),
+                new Fraction(3).add(new Fraction(0)));
     }
 
     @Test
     public void zeroPlusNonZero() throws Exception {
-        Fraction sum = new Fraction(0).add(new Fraction(7));
-        assertEquals(7, sum.intValue());
+        assertEquals(
+                new Fraction(7),
+                new Fraction(0).add(new Fraction(7)));
     }
 
     @Test
     public void nonNegativeNonZeroOperands() throws Exception {
-        Fraction sum = new Fraction(3).add(new Fraction(2));
-        assertEquals(5, sum.intValue());
+        assertEquals(
+                new Fraction(5),
+                new Fraction(3).add(new Fraction(2)));
     }
 
     @Test
     public void negativeInputsAndNegativeOutput() throws Exception {
-        Fraction sum = new Fraction(-2).add(new Fraction(1));
-        assertEquals(-1, sum.intValue());
+        assertEquals(
+                new Fraction(-1),
+                new Fraction(-2).add(new Fraction(1)));
     }
 
     @Test
     public void nonTrivialButCommonDenominator() throws Exception {
-        Fraction sum = new Fraction(1, 5).add(new Fraction(2, 5));
-        assertEquals(new Fraction(3, 5), sum);
+        assertEquals(
+                new Fraction(3, 5),
+                new Fraction(1, 5).add(new Fraction(2, 5)));
     }
 
     @Test
     public void commonDenominatorIsOne() throws Exception {
-        Fraction sum = new Fraction(2, 1).add(new Fraction(3, 1));
-        assertEquals(5, sum.getNumerator());
-        assertEquals(1, sum.getDenominator());
+        assertEquals(
+                new Fraction(5, 1),
+                new Fraction(2, 1).add(new Fraction(3, 1)));
     }
 }
