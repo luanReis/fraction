ac513d48bea602e916962a5cdf78ce467141126d
Luan Reis <luan.rpsantos@gmail.com>
2017-04-20 23:08:44 +0200

    Change behaviour of GCD algorithm to return always positive

--- test/source/com/luanreis/math/test/GreatestCommonDivisorTest.java
+++ test/source/com/luanreis/math/test/GreatestCommonDivisorTest.java
@@ -11,14 +11,14 @@ public class GreatestCommonDivisorTest {
         assertEquals(0, gcd(0, 0));
         assertEquals(1, gcd(1, 1));
         assertEquals(2, gcd(2, 2));
-        assertEquals(-1, gcd(-1, -1));
+        assertEquals(1, gcd(-1, -1));
     }
 
     @Test
     public void relativelyPrime() throws Exception {
         assertEquals(1, gcd(2, 3));
         assertEquals(1, gcd(4, 7));
-        assertEquals(-1, gcd(-2, -3));
+        assertEquals(1, gcd(-2, -3));
     }
 
     @Test
@@ -32,7 +32,7 @@ public class GreatestCommonDivisorTest {
     public void commonFactor() throws Exception {
         assertEquals(2, gcd(6, 8));
         assertEquals(7, gcd(49, 315));
-        assertEquals(-4, gcd(-24, -28));
+        assertEquals(4, gcd(-24, -28));
     }
 
     private int gcd(int a, int b) {
@@ -41,6 +41,6 @@ public class GreatestCommonDivisorTest {
             b = a % t;
             a = t;
         }
-        return a;
+        return Math.abs(a);
     }
 }
