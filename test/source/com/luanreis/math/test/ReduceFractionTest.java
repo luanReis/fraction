94329369d595a0724a0e107e1eb9aa9a28fd1afe
Luan Reis <luan.rpsantos@gmail.com>
2017-04-27 14:29:31 +0200

    Add more tests for reducing fractions

--- test/source/com/luanreis/math/test/ReduceFractionTest.java
+++ test/source/com/luanreis/math/test/ReduceFractionTest.java
@@ -13,4 +13,18 @@ public class ReduceFractionTest {
                 new Fraction(3, 4),
                 new Fraction(6, 8));
     }
+
+    @Test
+    public void reduceToWholeNumber() throws Exception {
+        assertEquals(
+                new Fraction(6),
+                new Fraction(24, 4));
+    }
+
+    @Test
+    public void reduceToZero() throws Exception {
+        assertEquals(
+                new Fraction(0),
+                new Fraction(0, 128349));
+    }
 }
